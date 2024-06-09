9e7e0055e886cdcb10f4d737158c1305f2361ac2
Dude <156313370+jacobBateman@users.noreply.github.com>
2024-06-09 16:24:14 +0200

    Add files via upload

--- site/src/main/java/auth/AccountManager.java
+++ site/src/main/java/auth/AccountManager.java
@@ -1,8 +1,30 @@
 package auth;
 
+import org.mindrot.jbcrypt.BCrypt;
+import database.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class AccountManager {
+    private final DatabaseConnection dbConnection;
 
-    public void register(){
+    public AccountManager(DatabaseConnection dbConnection) {
+        this.dbConnection = dbConnection;
+    }
 
+    public boolean register(String username, String password){
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+        try(
+            Connection con = dbConnection.getConnection();
+            PreparedStatement pstmt = con.prepareStatement("INSERT INTO users (username, password) VALUES (?,?)")){
+            pstmt.setString(1,username);
+            pstmt.setString(2,hashedPassword);
+            pstmt.executeUpdate();
+            return true;
+
+        }catch(java.sql.SQLException e){
+            return false;
+        }
     }
 }
